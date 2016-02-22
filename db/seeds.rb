22727392716f5b4c4410ebbccd42a0dcc604d70c
Skitelman <sghitelman@gmail.com>
2016-02-21 23:10:52 -0500

    added friendships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Create Users"
+
+sam = User.create(name: "Sam")
+kiron = User.create(name: "Kiron")
+kathleen = User.create(name: "Kathleen")
+zack = User.create(name: "Zack")
+pete = User.create(name: "Pete")
+gabe = User.create(name: "Gabe")
+david = User.create(name: "David")
+ethan = User.create(name: "Ethan")
+
+puts "Sam sends out friend requests"
+
+sam_kiron = Friendship.create(user: sam, friend: kiron, confirmed: true)
+sam_kathleen = Friendship.create(user: sam, friend: kathleen, confirmed: true)
+sam_zack = Friendship.create(user: sam, friend: zack)
+sam_pete = Friendship.create(user: sam, friend: pete)
+sam_gabe = Friendship.create(user: sam, friend: gabe)
+sam_david = Friendship.create(user: sam, friend: david)
+sam_ethan = Friendship.create(user: sam, friend: ethan)
